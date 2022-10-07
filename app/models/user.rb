d3281cc1a0bcc75b3a6d5a2786ac47eb329b1004
mikihiro makino <111714413+mikimm@users.noreply.github.com>
2022-10-07 16:26:31 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true, length: { maximum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
   has_many :tasks
 end
